eda91458fc64e9e17329cac5ee7fb77e9d189f8b
MavNZ <85376209+MavNZ@users.noreply.github.com>
2022-04-29 00:57:55 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MyTerminal from './terminal'
+
+function App() {
+  return (
+    // <main>
+    //   React⚛️ + Vite⚡ + Replit🌀
+      
+    // </main>
+    <MyTerminal />
+  );
+}
+
+export default App;
---
+++ src/terminal.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Terminal from 'react-console-emulator'
+
+const name = 'Riley'
+
+const commands = {
+  echo: {
+    description: 'Echo a passed string.',
+    usage: 'echo <string>',
+    fn: function () {
+      return `${Array.from(arguments).join(' ')}`
+    }
+  },
+  pronouns: {
+    description: 'Displays preferred pronouns',
+    usage: 'pronouns',
+    fn: function() {
+      return `${name} uses she/her pronouns`
+    }
+  }
+}
+
+export default class MyTerminal extends Component {
+  render () {
+    return (
+      <Terminal
+        commands={commands}
+        welcomeMessage={'Welcome to the React terminal!'}
+        promptLabel={'me@React:~$'}
+      />
+    )
+  }
+}
