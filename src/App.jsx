909a3c0ead67f6cbaf57e8662b20ed26542761c2
Chris Johnson <christopherjohnsonnz@gmail.com>
2022-04-30 23:36:20 +0100

    added 4th terminal

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import {
   Routes, Route, Link 
 } from "react-router-dom"
 import './App.css'
-import MyTerminal from './terminal'
-import MyComponent from './terminal2'
-import ReactTerminal from './terminal3'
+import MyTerminal from './terminals/1/terminal'
+import MyComponent from './terminals/2/terminal2'
+import ReactTerminal from './terminals/3/terminal3'
+import Terminal4 from './terminals/4/terminal4'
 
 function App() {
 	return (
@@ -15,9 +16,10 @@ function App() {
         <nav>
           <div className="container space-around">
             <Link to="/">Home</Link>
-            <Link to="/terminal1">Terminal</Link>
+            <Link to="/terminal1">Terminal1</Link>
             <Link to="/terminal2">Terminal2</Link>
             <Link to="/terminal3">Terminal3</Link>
+            <Link to="/terminal4">Terminal4</Link>
           </div>
         </nav>
 
@@ -25,6 +27,7 @@ function App() {
           <Route path="/terminal1" element={<MyTerminal />} />
           <Route path="/terminal2" element={<MyComponent />} />
           <Route path="/terminal3" element={<ReactTerminal />} />
+          <Route path="/terminal4" element={<Terminal4 />} />
           <Route path="/" element={<h1>Terminal Test!!!</h1>} />
         </Routes>
       </div>
---
+++ src/terminals/4/terminal4.jsx
@@ -0,0 +1,73 @@
+import React, { useState, Component } from 'react'
+import ReactConsole from '@webscopeio/react-console'
+import './terminal4.css'
+
+export default class Terminal4 extends Component {
+  constructor (props) {
+    super(props)
+    this.terminal = React.createRef()
+    this.state = {
+      history: [
+        "echo hello world",
+        "sleep 1000",
+        "sleep 2000",
+        "sleep 3000",
+        "echo ola",
+        "not found",
+      ]
+    }
+  }
+
+  componentDidMount() {
+    console.log('mounted', this)
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactConsole
+          ref={this.terminal}
+          autoFocus
+          welcomeMessage="Welcome"
+          commands={{
+            help: {
+              description: 'Help',
+              fn: () => {
+                console.log('here', this.terminal.current.props.commands)
+                const commands = Object.keys(this.terminal.current.props.commands).join('\n')
+                return new Promise(resolve => resolve(`${commands}`))
+              }
+            },
+            history: {
+              description: 'History',
+              fn: () => new Promise(resolve => {
+                 resolve(`${this.state.history.join('\r\n')}`)
+              })
+            },
+            echo: {
+              description: 'Echo',
+              fn: (...args) => {
+                return new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    resolve(`${args.join(' ')}`)
+                  }, 2000)
+                })
+              }
+            },
+            test: {
+              description: 'Test',
+              fn: (...args) => {
+                return new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    resolve('Hello world \n\n hello \n')
+                  }, 2000)
+                })
+              }
+            }
+          }}
+        />
+      </div>
+    )
+  }
+
+}
